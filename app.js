0184067903b3e4a763f504146ef5f73077893316
fadhilsurya <fadhilsurya1@gmail.com>
2019-07-31 17:52:01 +0700

    sandbox

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const routes = require('./routes')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(express.json())
 app.use('/', routes)
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening to the port ${port}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -2,61 +2,61 @@ const routes = require('express').Router()
 const Model = require('../models')
 
 // go to HomePage
-routes.get('/', (req,res)=>{
-    res.send('Masuk Pak Ekoooo')
-})
-// go to HomePage
+routes.get('/', (req, res) => {
+        res.render('home')
+    })
+    // go to HomePage
 
 // go to RegisterPage
-routes.get('/register', (req,res)=>{
-    res.render('halamanRegister????')
-})
-// go to RegisterPage
+routes.get('/register', (req, res) => {
+        res.render('halamanRegister????')
+    })
+    // go to RegisterPage
 
 // create new Customer
-routes.post('/register', (req,res)=>{
-    Model.Customer.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+routes.post('/register', (req, res) => {
+        Model.Customer.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then(() => {
+                res.send('Syudah daptarr')
+            })
+            .catch(err => {
+                res.send(err)
+            })
     })
-    .then(()=>{
-        res.send('Syudah daptarr')
-    })
-    .catch(err =>{
-        res.send(err)
-    })
-})
-// create new Customer
+    // create new Customer
 
 // go to loginPage
-router.get('/login', (req,res)=>{
-    Model.Customer.findOne({
-        where: {
-            email: req.body.email
-        }
+routes.get('/login', (req, res) => {
+        Model.Customer.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then(data => {
+                if (data.password == req.body.password) {
+                    res.send('Halaman User..')
+                }
+            })
+            .catch(err => {
+                res.send(err)
+            })
     })
-    .then(data =>{
-        if(data.password == req.body.password){
-            res.send('Halaman User..')
-        }
-    })
-    .catch(err =>{
-        res.send(err)
-    })
-})
-// go to loginPage
+    // go to loginPage
 
 
-router.get('/login/:id/transaction', (req,res)=>{
+routes.get('/login/:id/transaction', (req, res) => {
     Model.Transaction.create({
-        CustomerId: req.params.id
-    })
-    .then(()=>{
+            CustomerId: req.params.id
+        })
+        .then(() => {
 
-    })
+        })
 })
 
-router.get('/')
+routes.get('/')
 
 module.exports = routes
