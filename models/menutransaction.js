b04307f204e00902daa9267664b129cea16d2e25
VincentiusPramudito <vpramudito@yahoo.com>
2019-07-31 13:36:39 +0700

    addModelMenuTransaction

---
+++ models/menutransaction.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Model = sequelize.Sequelize.Model
+  class MenuTransaction extends Model{}
+  MenuTransaction.init ({
+    TransactionId: DataTypes.INTEGER,
+    MenuId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER
+  }, {sequelize});
+  MenuTransaction.associate = function(models) {
+    // associations can be defined here
+  };
+  return MenuTransaction;
+};
